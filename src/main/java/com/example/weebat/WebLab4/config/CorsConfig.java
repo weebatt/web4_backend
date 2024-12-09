bf717d1fb1900b5180ef795dfc03bc19de835c54
Alexandrov Alexandr <144331961+butareyka@users.noreply.github.com>
2024-12-09 15:34:11 +0300

    back to simple addCorsMappings

--- src/main/java/com/example/weebat/WebLab4/config/CorsConfig.java
+++ src/main/java/com/example/weebat/WebLab4/config/CorsConfig.java
@@ -1,19 +1,17 @@
 package com.example.weebat.WebLab4.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://readme.weebat.ru")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
