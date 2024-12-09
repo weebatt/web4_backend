536f5284f451c78c28afa7d2efa5894ef37b1506
Alexandrov Alexandr <144331961+butareyka@users.noreply.github.com>
2024-12-09 15:27:54 +0300

    turn off cors in WebMvcConfigurer

--- src/main/java/com/example/weebat/WebLab4/config/CorsConfig.java
+++ src/main/java/com/example/weebat/WebLab4/config/CorsConfig.java
@@ -1,17 +1,19 @@
 package com.example.weebat.WebLab4.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://readme.weebat.ru")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 }
