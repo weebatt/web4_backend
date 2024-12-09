d08433ce92d6fe4362df2c02c288490e061569ab
Alexandrov Alexandr <144331961+butareyka@users.noreply.github.com>
2024-12-09 14:28:51 +0300

    update corsConfigurer

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
-FROM tomcat:9.0.70-jdk11-openjdk
+FROM tomcat:9-jdk11
 
 WORKDIR /usr/local/tomcat/webapps/
 
-COPY target/*.war app.war
+COPY build/libs/*.war app.war
 
 EXPOSE 8080
 
--- src/main/java/com/example/weebat/WebLab4/config/SecurityConfig.java
+++ src/main/java/com/example/weebat/WebLab4/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://localhost:4173") // Адрес вашего фронтенда
+                        .allowedOrigins("http://readme.weebat.ru:80") // Адрес вашего фронтенда
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
